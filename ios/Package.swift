9203fad364799662a5c5454f2469586303566b98
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-04-09 20:22:19 +0300

    Add Swift Package Manager support

---
+++ ios/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "zendesk_messaging",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(name: "zendesk-messaging", targets: ["zendesk_messaging"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/zendesk/sdk_messaging_ios", exact: "2.36.0")
+    ],
+    targets: [
+        .target(
+            name: "zendesk_messaging",
+            dependencies: [
+                .product(name: "ZendeskSDKMessaging", package: "sdk_messaging_ios")
+            ],
+            path: "Classes"
+        )
+    ]
+)
